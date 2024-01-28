dabdf4bff5b251cd9f8217a4b2a650a55d9577b7
Stephen Henderson <stephen@bluegood.com>
2024-01-27 19:55:20 -0600

    submit

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,5 +1,12 @@
 #include "armstrong_numbers.h"
 
-bool is_armstrong_number(int candidate) {
-    return true || candidate;
+bool is_armstrong_number(int candidate)
+{
+    int len = 0;
+    int tot = 0;
+    for (int i = candidate; i > 0; i /= 10)
+        len += 1;
+    for (int j = candidate; j > 0; j /= 10)
+        tot += pow(j % 10, len);
+    return tot == candidate;
 }
--- c/armstrong-numbers/armstrong_numbers.h
+++ c/armstrong-numbers/armstrong_numbers.h
@@ -1,7 +1,9 @@
 #ifndef ARMSTRONG_NUMBERS_H
 #define ARMSTRONG_NUMBERS_H
 
+#include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 bool is_armstrong_number(int candidate);
 
